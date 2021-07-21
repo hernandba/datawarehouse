d350936c64643044956edc6d538cc84d8f59577b
Hernan Belalcazar <hernandba@gmail.com>
2021-07-21 18:30:52 -0500

    Changed apiCall -> Added fetchParams with body and no-body

--- frontend/scripts/apiCall.js
+++ frontend/scripts/apiCall.js
@@ -1,16 +1,26 @@
 const baseApiUrl = 'http://localhost:3000';
 
-const apiCall = async (apiUrl, method, body, token) => {
-    token = typeof(token) !== 'undefined' ? token : ''; 
-    try {
-        const response = await fetch(apiUrl, {
+const apiCall = async (apiUrl, method, token, body) => {
+
+    const fetchParams = typeof (body) !== 'undefined' ? 
+        {
             method: method,
             body: JSON.stringify(body),
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': token
+                'Authorization': `BEARER ${token}`
             }
-        });
+        } 
+        :
+        {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `BEARER ${token}`
+            }
+        };
+    try {
+        const response = await fetch(apiUrl, fetchParams);
         const json = await response.json();
         return json;
     } catch (error) {
