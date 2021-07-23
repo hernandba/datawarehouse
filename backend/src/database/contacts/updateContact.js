4d28ac54e0cce4f748404c79e22f8c725f9035f5
Hernan Belalcazar <hernandba@gmail.com>
2021-07-23 16:02:07 -0500

    Erased comments on updateContact

--- backend/src/database/contacts/updateContact.js
+++ backend/src/database/contacts/updateContact.js
@@ -6,12 +6,6 @@ async function updateContact(id, data){
         let filteredData = dataToArray.filter(element => element[1] !== '') //Elimino del arreglo quienes tengan value = ''
         let mappedData = filteredData.map(element => {
             let value = typeof element[1] !== 'number' ? "'"+element[1]+"'" : element[1];
-            // let value;
-            // if(typeof element[1] !== 'number'){ //Si el value es distinto de un número
-            //     value = "'" + element[1] + "'"; //Agrego comillas al 'value'
-            // }else{
-            //     value = element[1]//Si no, dejo el value tal cual (numero)
-            // }
             return element[0] + ' = ' + value //Retorno pares de la forma key = 'value' ó key = value 
         })
         let finalData = mappedData.toString() //Convierto todo lo anterior en un string separado por comas
