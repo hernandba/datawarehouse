851812015a8deac506491f5e203303e99d12ca66
Hernan Belalcazar <hernandba@gmail.com>
2021-08-03 19:10:18 -0500

    Fron /contacts search and delete (multiple selected, unitary)

--- frontend/scripts/contacts/contacts_main.js
+++ frontend/scripts/contacts/contacts_main.js
@@ -6,23 +6,103 @@ const navUsersLink = document.querySelector('#nav-users');
 let contactRecord = document.querySelector('.contact-record');
 let contactsRecordsContainer = document.querySelector('#contacts-records-container');
 
-if(validateCredential()){
+let inputSearch = document.querySelector('#inputSearch');
+let btnSearchContact = document.querySelector('#btnSearchContact');
+
+let btnDeleteContactsSelected = document.querySelector('#btnDeleteContactsSelected')
+let checkAllContacts = document.querySelector('#checkAllContacts');
+
+let contactsSelected = 0;
+let contactsToDelete = [];
+
+if (validateCredential()) {
     userProfile = sessionCredentials.profile;
     userToken = sessionCredentials.token;
-    
+
     /* ---------------------------- Link de usuarios ---------------------------- */
-    if(userProfile !== 'admin'){
+    if (userProfile !== 'admin') {
         navUsersLink.classList.add('d-none')
     }
 
+    /* ----------------------------- SEARCH CONTACTS ---------------------------- */
+    //ENTER Key
+    inputSearch.addEventListener('keyup', event => {
+        if (event.keyCode == 13) {
+            if (inputSearch.value !== '') {
+                contactsRecordsContainer.innerHTML = '';
+                apiCall(`${baseApiUrl}/contacts?search=${inputSearch.value}`, 'GET', userToken).then(response => {
+                    loadContactsRecords(response.data, contactsRecordsContainer, contactRecord)
+                }).catch(error => console.error(error))
+            } else {
+                contactsRecordsContainer.innerHTML = '';
+                apiCall(`${baseApiUrl}/contacts`, 'GET', userToken).then(response => {
+                    loadContactsRecords(response.data, contactsRecordsContainer, contactRecord)
+                }).catch(error => console.error(error))
+            }
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    })
+    //CLICK ON Button 
+    btnSearchContact.addEventListener('click', event => {
+        if (inputSearch.value !== '') {
+            contactsRecordsContainer.innerHTML = '';
+            apiCall(`${baseApiUrl}/contacts?search=${inputSearch.value}`, 'GET', userToken).then(response => {
+                loadContactsRecords(response.data, contactsRecordsContainer, contactRecord)
+            }).catch(error => console.error(error))
+        } else {
+            contactsRecordsContainer.innerHTML = '';
+            apiCall(`${baseApiUrl}/contacts`, 'GET', userToken).then(response => {
+                loadContactsRecords(response.data, contactsRecordsContainer, contactRecord)
+            }).catch(error => console.error(error))
+        }
+
+        event.preventDefault();
+        event.stopPropagation();
+    })
+
     /* ----------------------------- CREATE contacts ---------------------------- */
 
+    /* ----------------------------- DELETE contacts ---------------------------- */
+    //CHECK (SELECT-UNSELECT) all contacts
+    checkAllContacts.addEventListener('click', event => {
+        if(checkAllContacts.checked == true){
+            contactsToDelete = [];
+            document.querySelectorAll('.contact-check').forEach(contactCheck => {
+                contactCheck.checked = true;
+                contactsToDelete.push(contactCheck.value)
+                contactsSelected = document.querySelectorAll('.contact-check').length;
+
+                btnDeleteContactsSelected.classList.remove('d-none')
+
+                console.log('contactsToDelete: '+contactsToDelete);
+                console.log('contactsSelected: '+contactsSelected);
+            })
+        }else{
+            contactsToDelete = [];
+            document.querySelectorAll('.contact-check').forEach(contactCheck => {
+                contactCheck.checked = false;
+                contactsSelected = 0;
+                console.log('contactsToDelete: '+contactsToDelete);
+                console.log('contactsSelected: '+contactsSelected);
+
+                btnDeleteContactsSelected.classList.add('d-none')
+            })
+        }
+    })
+    //DELETE CONTACTS SELECTED Button
+    btnDeleteContactsSelected.addEventListener('click', event => {
+        if(confirm('Esta seguro de que desea eliminar los contactos seleccionado(s)?')){
+            apiCall(`${baseApiUrl}/contacts?ids=${contactsToDelete.toString()}`, 'DELETE', userToken).then(response => {
+                alert('Contactos eliminados');
+                location.reload();
+            }).catch(error => console.error(error))
+        }
+    })
 
     /* ------------------------------ READ contacts ----------------------------- */
     //LOAD contacts to table
     apiCall(`${baseApiUrl}/contacts`, 'GET', userToken).then(response => {
         loadContactsRecords(response.data, contactsRecordsContainer, contactRecord)
-
     }).catch(error => console.error(error))
 }
-
--- frontend/scripts/contacts/loadContactsRecords.js
+++ frontend/scripts/contacts/loadContactsRecords.js
@@ -6,6 +6,7 @@ function loadContactsRecords(data, entriesContainer, recordTemplate){
         newContactRecord.setAttribute('class', 'contact-record');
         newContactRecord.innerHTML = recordTemplate.innerHTML;
 
+        newContactRecord.querySelector('.contact-check').value = id;
         newContactRecord.querySelector('.contact-fullname').innerText = name+' '+lastname;
         newContactRecord.querySelector('.contact-email').innerText = email;
         newContactRecord.querySelector('.contact-country').innerText = country;
@@ -14,18 +15,54 @@ function loadContactsRecords(data, entriesContainer, recordTemplate){
         newContactRecord.querySelector('.contact-role').innerText = role;
         newContactRecord.querySelector('.contact-interest-number').innerText = interested+'%';
 
+        newContactRecord.querySelector('.contact-interest-ruler').style.width = interested+'%';
+
+        let contactCheck = newContactRecord.querySelector('.contact-check');
+
+        contactCheck.addEventListener('click', event => {
+            if(contactCheck.checked == true){
+                contactsSelected++;
+                contactsToDelete.push(contactCheck.value);
+                btnDeleteContactsSelected.classList.remove('d-none')
+                console.log('contactsToDelete: '+contactsToDelete);
+                console.log('contactsSelected: '+contactsSelected);
+            }else{
+                contactsSelected--;
+                contactsToDelete.splice(contactsToDelete.indexOf(contactCheck.value),1)
+                if(contactsSelected == 0){
+                    btnDeleteContactsSelected.classList.add('d-none')
+                }
+                console.log('contactsToDelete: '+contactsToDelete);
+                console.log('contactsSelected: '+contactsSelected);
+            }
+        })
+
+        /* ------------------------------ ACTION BUTTONS ------------------------------ */
         let fieldActionsDisplayBtn = newContactRecord.querySelector('.field-actions-display')
         let fieldActionsBtns = newContactRecord.querySelector('.field-actions-buttons')
-
+        //Click on 3 points to show
         fieldActionsDisplayBtn.addEventListener('click', event => {
             fieldActionsDisplayBtn.classList.toggle('d-none');
             fieldActionsBtns.classList.toggle('d-none')
             event.stopPropagation();
         })
-
+        //Mouse out to hide
         fieldActionsBtns.addEventListener('mouseout', event => {
             fieldActionsDisplayBtn.classList.toggle('d-none');
             fieldActionsBtns.classList.toggle('d-none')
+            event.stopPropagation();
+        })
+
+        /* ------------------------------ ERASE BUTTON ------------------------------ */
+        let eraseBtn = newContactRecord.querySelector('.field-erase');
+        eraseBtn.addEventListener('click', event => {
+            let eraseConfirm = confirm('Está seguro de que desea eliminar este contacto?')
+            if (eraseConfirm) {
+                apiCall(`${baseApiUrl}/contacts?ids=${id}`, 'DELETE', userToken).then(response => {
+                    alert('Contacto eliminado');
+                    location.reload();
+                })
+            }
             event.stopPropagation();
         })
 
